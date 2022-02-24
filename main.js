2d0488bcf996564ac019efab116453cd737cfb5c
laurpe <laura.perttila@edu.bc.fi>
2022-02-24 10:56:45 +0200

    make title required field when adding task

--- main.js
+++ main.js
@@ -6,22 +6,20 @@ class Task {
     }
 }
 
+const title = document.querySelector("#title");
+const description = document.querySelector("#description");
+const isImportant = document.querySelector("#isImportant");
+
+title.setAttribute("required", "");
+
 const addTask = (event) => {
     event.preventDefault();
 
-    const title = document.querySelector("#title").value;
-    const description = document.querySelector("#description").value;
-    const isImportant = document.querySelector("#isImportant").checked;
-
-    const task = new Task(title, description, isImportant);
-
-    console.log(task);
+    const task = new Task(title.value, description.value, isImportant.checked);
 
     let tasks = [];
 
     tasks.push(task);
-
-    console.log(tasks);
 
     const taskTable = document.querySelector("#task-table");
     const row = document.createElement("tr");
@@ -31,11 +29,10 @@ const addTask = (event) => {
         })
         .join("");
 
-    console.log(taskTable);
-    console.log(taskData);
-
     row.innerHTML = taskData;
     taskTable.appendChild(row);
+
+    event.target.reset();
 };
 
 const addTaskForm = document.querySelector("#add-task-form");
