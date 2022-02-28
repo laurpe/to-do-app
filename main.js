eae067a7e89a8cf5fdeecca3a2835d155567b4db
laurpe <laura.perttila@edu.bc.fi>
2022-02-28 17:01:05 +0200

    deleted description field, added date

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 class Task {
-    constructor(title, description, isImportant) {
+    constructor(title, isImportant, dateAdded) {
         this.title = title;
-        this.description = description;
         this.isImportant = isImportant;
+        this.dateAdded = dateAdded;
     }
 }
 
 const title = document.querySelector("#title");
-const description = document.querySelector("#description");
 const isImportant = document.querySelector("#isImportant");
 
+const taskList = document.querySelector("#task-list");
+
 title.setAttribute("required", "");
+
+let tasks = [];
 
 const addTask = (event) => {
     event.preventDefault();
 
-    const task = new Task(title.value, description.value, isImportant.checked);
-
-    let tasks = [];
+    const task = new Task(
+        title.value,
+        isImportant.checked,
+        new Date().toDateString()
+    );
 
     tasks.push(task);
 
@@ -26,10 +31,8 @@ const addTask = (event) => {
     event.target.reset();
 };
 
-const taskTable = document.querySelector("#task-table");
-
 const createList = (tasks) => {
-    const taskList = document.querySelector("#task-list");
+    taskList.innerHTML = "";
 
     for (let i = 0; i < tasks.length; i++) {
         taskList.insertAdjacentHTML(
@@ -38,37 +41,28 @@ const createList = (tasks) => {
             <li>
                 <div class="task-element">
                     <div class="task-name">${tasks[i].title}</div>
-                    <div class="task-description">${tasks[i].description}</div>
                     <div class="task-importance">${tasks[i].isImportant}</div>
+                    <div class="task-importance">${tasks[i].dateAdded}</div>
+                    <div class="btn-remove-tasks">
+                        <button>remove</button>
+                        </div>
                 </div>
             </li>
         `
         );
-
-        // const taskElement = document.createElement("div");
-        // taskElement.classList.add("task-element");
-        // const taskName = document.createElement("div");
-        // taskName.classList.add("task-name");
-        // const taskDescription = document.createElement("div");
-        // taskDescription.classList.add("task-description");
-        // const taskImportance = document.createElement("div");
-        // taskImportance.classList.add("task-importance");
-
-        // taskElement.appendChild(taskName);
-        // taskElement.appendChild(taskDescription);
-        // taskElement.appendChild(taskImportance);
-
-        // taskName.innerText = tasks[i].title;
-        // taskDescription.innerText = tasks[i].description;
-        // taskImportance.innerText = tasks[i].isImportant;
-
-        // const li = document.createElement("li");
-        // li.appendChild(taskElement);
-
-        // taskList.appendChild(li);
     }
 
     console.log(taskList);
+};
+
+const deleteTask = (task) => {
+    tasks = tasks.filter((item) => {
+        if (!item === task) {
+            return item;
+        }
+    });
+
+    createList(tasks);
 };
 
 const addTaskForm = document.querySelector("#add-task-form");
