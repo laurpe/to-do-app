7d5de6f4edbd1721f6a2e1f6e72aaae41cda5fc9
laurpe <laura.perttila@edu.bc.fi>
2022-02-28 19:26:08 +0200

    star checkbox, remove button styling

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ const addTask = (event) => {
     const task = new Task(
         title.value,
         isImportant.checked,
-        new Date().toDateString()
+        new Date().toLocaleString()
     );
 
     tasks.push(task);
@@ -41,11 +41,13 @@ const createList = (tasks) => {
             `
             <li>
                 <div class="task-element">
-                    <div class="task-name">${tasks[i].title}</div>
+                    <div class="task-title">${tasks[i].title}</div>
                     <div class="task-importance">${tasks[i].isImportant}</div>
                     <div class="task-importance">${tasks[i].dateAdded}</div>
                     <div class="remove-task">
-                        <button class="btn-remove-task">remove</button>
+                        <button class="btn-remove-task"><span class="material-icons remove-icon">
+                        clear
+                        </span></button>
                     </div>
                 </div>
             </li>
@@ -69,10 +71,28 @@ addTaskForm.addEventListener("submit", addTask);
 
 document.addEventListener("click", (event) => {
     if (event.target.matches(".btn-remove-task")) {
-        console.log("clicked remove button");
         deleteTask(
             event.target.parentNode.parentNode.firstElementChild.textContent
         );
         event.target.parentNode.parentNode.parentNode.remove();
+    } else if (event.target.matches(".remove-icon")) {
+        deleteTask(
+            event.target.parentNode.parentNode.parentNode.firstElementChild
+                .textContent
+        );
+        event.target.parentNode.parentNode.parentNode.parentNode.remove();
+    }
+});
+
+// star checkbox
+const starbox = document.querySelector("#starbox");
+
+isImportant.addEventListener("change", function () {
+    if (this.checked) {
+        console.log("Checkbox is checked..");
+        starbox.textContent = "star";
+    } else {
+        console.log("Checkbox is not checked..");
+        starbox.textContent = "star_outline";
     }
 });
