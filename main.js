8c82f81157cea566418bce49bb6dd0e8046e10a1
laurpe <laura.perttila@edu.bc.fi>
2022-02-28 13:59:02 +0200

    two ways of creating list

--- main.js
+++ main.js
@@ -21,22 +21,54 @@ const addTask = (event) => {
 
     tasks.push(task);
 
-    createTable(tasks);
+    createList(tasks);
 
     event.target.reset();
 };
 
-const createTable = (tasks) => {
-    const taskTable = document.querySelector("#task-table");
-    const row = document.createElement("tr");
-    const taskData = tasks
-        .map((task) => {
-            return `<td>${task.title}</td><td>${task.description}</td><td>${task.isImportant}</td>`;
-        })
-        .join("");
+const taskTable = document.querySelector("#task-table");
 
-    row.innerHTML = taskData;
-    taskTable.appendChild(row);
+const createList = (tasks) => {
+    const taskList = document.querySelector("#task-list");
+
+    for (let i = 0; i < tasks.length; i++) {
+        taskList.insertAdjacentHTML(
+            "beforeend",
+            `
+            <li>
+                <div class="task-element">
+                    <div class="task-name">${tasks[i].title}</div>
+                    <div class="task-description">${tasks[i].description}</div>
+                    <div class="task-importance">${tasks[i].isImportant}</div>
+                </div>
+            </li>
+        `
+        );
+
+        // const taskElement = document.createElement("div");
+        // taskElement.classList.add("task-element");
+        // const taskName = document.createElement("div");
+        // taskName.classList.add("task-name");
+        // const taskDescription = document.createElement("div");
+        // taskDescription.classList.add("task-description");
+        // const taskImportance = document.createElement("div");
+        // taskImportance.classList.add("task-importance");
+
+        // taskElement.appendChild(taskName);
+        // taskElement.appendChild(taskDescription);
+        // taskElement.appendChild(taskImportance);
+
+        // taskName.innerText = tasks[i].title;
+        // taskDescription.innerText = tasks[i].description;
+        // taskImportance.innerText = tasks[i].isImportant;
+
+        // const li = document.createElement("li");
+        // li.appendChild(taskElement);
+
+        // taskList.appendChild(li);
+    }
+
+    console.log(taskList);
 };
 
 const addTaskForm = document.querySelector("#add-task-form");
